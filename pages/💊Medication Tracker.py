f16ac943f89e6098b0107d05e6a8938ef02a0dea
Zarik Khan <70036299+ZK333@users.noreply.github.com>
2023-02-17 10:03:34 -0600

    Update 💊Medication Tracker.py

--- pages/💊Medication Tracker.py
+++ pages/💊Medication Tracker.py
@@ -1,34 +1,25 @@
 import streamlit as st
 import pandas as pd
-med_data = pd.read_csv("medications.csv")
 
-def filter_data(search_term):
-    filtered_data = med_data[med_data['name'].str.contains(search_term, case=False)]
-    return filtered_data
-# Define a function to add a new medication to the medication data CSV file
-def add_medication(name, dose, frequency):
-    new_med = {'name': name, 'dose': dose, 'frequency': frequency}
-    med_data = med_data.append(new_med, ignore_index=True)
+st.set_page_config(page_title="Medication Tracker", page_icon=":pill:")
+med_data = pd.read_csv("medications.csv")
+my_meds = []
+search_term = st.sidebar.text_input("Search for a medication:")
+filtered_data = med_data[med_data['name'].str.contains(search_term, case=False)]
+st.write(filtered_data[['name', 'dose', 'frequency']])
+
+st.write("Add a new medication:")
+name = st.text_input("Medication name:")
+dose = st.text_input("Dose:")
+frequency = st.text_input("Frequency:")
+if st.button("Add"):
+    med_data = med_data.append({'name': name, 'dose': dose, 'frequency': frequency}, ignore_index=True)
     med_data.to_csv("medications.csv", index=False)
-    return med_data
-def app():
-    # Set the app title and icon
-    st.set_page_config(page_title="Medication Tracker", page_icon=":pill:")
-    # Set the app header
-    st.title("Medication Tracker")
-    # Add a search bar to filter the medication data by name
-    search_term = st.text_input("Search for a medication:")
-    filtered_data = filter_data(search_term)
-    # Display the filtered medication data
-    if not filtered_data.empty:
-        st.write(filtered_data[['name', 'dose', 'frequency']])
-    else:
-        st.write("No matching medications found.")
-    #add a new medication to the CSV file
-    st.header("Add a new medication")
-    name = st.text_input("Medication name:")
-    dose = st.text_input("Medication dose:")
-    frequency = st.text_input("Medication frequency:")
-    if st.button("Add medication"):
-        add_medication(name, dose, frequency)
-        st.success("Medication added!")
+    my_meds.append(name)
+
+st.write("Your Medications:")
+if len(my_meds) == 0:
+    st.write("No medications added.")
+else:
+    for med in my_meds:
+        st.write(med)

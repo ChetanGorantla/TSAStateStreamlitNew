39f47ac18e7cc64d23fe98bafeaed9b9da62a1e1
Zarik Khan <70036299+ZK333@users.noreply.github.com>
2023-02-17 12:42:09 -0600

    Update 💊Medication Tracker.py

--- pages/💊Medication Tracker.py
+++ pages/💊Medication Tracker.py
@@ -4,16 +4,16 @@ import pandas as pd
 st.set_page_config(page_title="Medication Tracker", page_icon=":pill:")
 med_data = pd.read_csv("medications.csv")
 my_meds = []
-search_term = st.sidebar.text_input("Search for a medication:")
+search_term = st.text_input("Search for a medication:")
 filtered_data = med_data[med_data['medication_name'].str.contains(search_term, case=False)]
-st.write(filtered_data[['medication_name', 'dose', 'frequency']])
+st.write(filtered_data[['medication_name', 'start_date', 'end_date']])
 
 st.write("Add a new medication:")
 name = st.text_input("Medication name:")
-dose = st.text_input("Dose:")
-frequency = st.text_input("Frequency:")
+start_date = st.text_input("Start date (yyyy-mm-dd):")
+end_date = st.text_input("End date (yyyy-mm-dd):")
 if st.button("Add"):
-    med_data = med_data.append({'medication_name': name, 'dose': dose, 'frequency': frequency}, ignore_index=True)
+    med_data = med_data.append({'medication_name': name, 'start_date': start_date, 'end_date': end_date}, ignore_index=True)
     med_data.to_csv("medications.csv", index=False)
     my_meds.append(name)
 
@@ -24,13 +24,13 @@ else:
     for med in my_meds:
         st.write(med)
 
-        
 streamlit_style = """
-			<style>
-			@import url('https://fonts.googleapis.com/css2?family=Raleway:ital@1&display=swap');
-			html, body, [class*="css"]  {
-			font-family: 'Raleway', sans-serif;
-			}
-			</style>
-			"""
+            <style>
+            @import url('https://fonts.googleapis.com/css2?family=Raleway:ital@1&display=swap');
+            html, body, [class*="css"]  {
+            font-family: 'Raleway', sans-serif;
+            }
+            </style>
+            """
 st.markdown(streamlit_style, unsafe_allow_html=True)
+

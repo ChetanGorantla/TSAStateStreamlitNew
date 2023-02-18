c4f8bd89fe6340ca856213707cfc0e96e7af2d28
Zarik Khan <70036299+ZK333@users.noreply.github.com>
2023-02-17 23:57:58 -0600

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -6,7 +6,7 @@ SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL=True
 
 st.image("Screenshot 2023-02-17 235252.png")
 st.write(
-    "Welcome! The Doctor is a univeral medical diagnostic tool with a range of functionality."
+    "Welcome! KGK Diagnosis is a univeral medical diagnostic tool with a range of functionality."
 )
 st.write(
     "Choose a page to the left most consistent with the particular problem you are having, and you can find more information on the individual screens."
@@ -24,14 +24,15 @@ footer:before{
 }
 </style>
 """
-streamlit_style = """
-			<style>
-			@import url('https://fonts.googleapis.com/css2?family=Raleway:ital@1&display=swap');
+streamlit_style = 
+"""
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Raleway:ital@1&display=swap');
 
-			html, body, [class*="css"]  {
-			font-family: 'Raleway', sans-serif;
-			}
-			</style>
-			"""
+html, body, [class*="css"]  {
+font-family: 'Raleway', sans-serif;
+}
+</style>
+"""
 st.markdown(streamlit_style, unsafe_allow_html=True)
 st.markdown(footer, unsafe_allow_html=True)

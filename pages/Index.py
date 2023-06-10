c1b38cde01cd6e805ecc28ed3052aa92ea00493a
ChetanGorantla <65378979+ChetanGorantla@users.noreply.github.com>
2023-06-10 18:23:44 -0500

    Update Index.py

--- pages/Index.py
+++ pages/Index.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-col1, col2, col3, col4 = st.columns(4)
+col1, col2, col3 = st.columns(4)
 
 with col1:
    st.header("A cat")
@@ -11,11 +11,11 @@ with col1:
 with col2:
    st.header("A dog")
    st.image("https://static.streamlit.io/examples/dog.jpg")
+   st.header("Doggies are very nice to play with because they aaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
 
 with col3:
    st.header("An owl")
    st.image("https://static.streamlit.io/examples/owl.jpg")
 
-with col4:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg")
+

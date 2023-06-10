98ac666ab009ec8b2fb93b328be6903d8ebe1637
ChetanGorantla <65378979+ChetanGorantla@users.noreply.github.com>
2023-06-10 18:57:09 -0500

    Update 🔍Index.py

--- pages/🔍Index.py
+++ pages/🔍Index.py
@@ -5,8 +5,11 @@ col1, col2, col3 = st.columns(3, gap = "medium")
 with col1:
    st.header("A cat")
    st.image("https://static.streamlit.io/examples/cat.jpg")
+   
    st.header("Fungal Infection")
-   st.text("Itching, soreness, redness or rash in the affected area. Discolored, thick or cracked nails. Pain while eating, loss of taste or white patches in mouth or throat. A painless lump under your skin.")
+   st.text("Symptoms include itching, soreness, redness or rash in the affected area. Discolored, thick or cracked nails. Pain while eating, loss of taste or white patches in mouth or throat. A painless lump under your skin.")
+   st.text("A Fungal Infection is a fungus that invades the tissue which can cause a disease that's confined to the skin, spreads into tissue, bones, and organs, or affects the whole body.")
+   st.text("To avoid attracting a Fungal Infection, keep your skin clean and dry, particularly the folds of your skin. Maintain good hygeine.")
    st.image("https://static.streamlit.io/examples/dog.jpg")
 
 with col2:

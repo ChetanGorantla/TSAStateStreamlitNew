af46740bf5f57f13855ee48ebf9bf67bf5b240e6
ChetanGorantla <65378979+ChetanGorantla@users.noreply.github.com>
2023-06-11 18:30:01 -0500

    Update 🔍Index.py

--- pages/🔍Index.py
+++ pages/🔍Index.py
@@ -10,56 +10,107 @@ with col1:
    st.image("https://static.streamlit.io/examples/dog.jpg")
    
    st.header("Allergy")
+   st.text("Symptoms include itchy, watery eyes, nose. Sneezing. Runny nose. Rashes.")
+   st.text("Allergies occur when your immune system reacts to a foreign substance.")
+   st.text("To avoid your allergies, you must void your allergens. Take your medicines as prescribed.")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Gerd")
+   st.text("Symptoms include burning pain in the chest that usually occurs after eating and worsens when lying down.")
+   st.text("This is a chronic disease that occurs when stomach acid or bile flows into the food pipe and irritates the lining.")
+   st.text("To avoid this, maintain a healthy weight. Stop smoking. Eat food thoroughly.")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Chronic cholestasis")
+   st.text("Symptoms include yellowing of your skin and the white of your eyes, dark urine, light-colored stool.")
+   st.text("Chronic cholestatic diseases, whether occurring in infancy, childhood or adulthood, are characterized by defective bile acid transport from the liver to the intestine.")
+   st.text("To avoid this, practice smoking and alcohol cessation, gentle weight bearing exercise regimen, and oral vitamin D and calcium supplements")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Drug Reaction")
+   st.text("Symptoms include hives, itching, skin rash, and swelling of the lips, tongue, or face.")
+   st.text("A drug reaction is a skin condition—such as an itchy or tender bump, rash, or blister—that develops when the body reacts adversely to medication.")
+   st.text("To avoid this, know why you are taking each medication. Know how to take the drug. Fill all your prescriptions at the same pharmacy.")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Peptic Ulcer Disease")
+   st.text("Symptoms include burning stomach pain. Feeling of fullness, bloating or belching. Intolerance to fatty foods.")
+   st.text("A sore that develops on the lining of the esophagus, stomach, or small intestine. Ulcers occur when stomach acid damages the lining of the digestive tract.")
+   st.text("To reduce risk of Peptic Ulcer Disease, avoid tobacco products. Avoid alcohol. Use caution with aspirin and/or NSAIDs.")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Typhoid")
+   st.text("Some common symptoms of typhoid include high fever, stomach pain, headache, and loss of appetite.")
+   st.text("Typhoid fever is a bacterial infection caused by Salmonella typhi and is characterized by a prolonged and high fever along with gastrointestinal symptoms.")
+   st.text("Some ways to prevent typhoid include practicing good hygiene, especially washing hands before eating or preparing food, and consuming safe and clean water and food")
    st.image("https://static.streamlit.io/examples/cat.jpg")
 
 with col2:
    st.header("AIDS")
+   st.text("")
+   st.text("")
+   st.text("")
    st.image("https://static.streamlit.io/examples/dog.jpg")
    
    st.header("Diabetes")
+   st.text("")
+   st.text("")
+   st.text("")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Gastroenteritis")
+   st.text("")
+   st.text("")
+   st.text("")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Bronchial Asthma")
+   st.text("")
+   st.text("")
+   st.text("")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Hypertension")
+   st.text("")
+   st.text("")
+   st.text("")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Migraine")
+   st.text("")
+   st.text("")
+   st.text("")
    st.image("https://static.streamlit.io/examples/cat.jpg")
 
 with col3:
    st.header("Cervical Spondylosis")
+   st.text("")
+   st.text("")
+   st.text("")
    st.image("https://static.streamlit.io/examples/owl.jpg")
    
    st.header("Paralysis")
+   st.text("")
+   st.text("")
+   st.text("")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Jaundice")
+   st.text("")
+   st.text("")
+   st.text("")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Malaria")
+   st.text("")
+   st.text("")
+   st.text("")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Chicken Pox")
+   st.text("")
+   st.text("")
+   st.text("")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Dengue")

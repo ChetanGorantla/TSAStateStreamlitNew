85b8489d6657ae881bc68630a920c210b9ba1a93
ChetanGorantla <65378979+ChetanGorantla@users.noreply.github.com>
2023-06-11 18:37:32 -0500

    Update 🔍Index.py

--- pages/🔍Index.py
+++ pages/🔍Index.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 col1, col2, col3 = st.columns(3, gap = "large")
-
+#source - Mayo Clinic, ChatGPT
 with col1:
    st.header("Fungal Infection")
    st.text("Symptoms include itching, soreness, redness or rash in the affected area. Discolored, thick or cracked nails. Pain while eating, loss of taste or white patches in mouth or throat. A painless lump under your skin.")
@@ -47,73 +47,77 @@ with col1:
 
 with col2:
    st.header("AIDS")
-   st.text("")
-   st.text("")
-   st.text("")
+   st.text("Symptoms include rapid weight loss, Recurring fever or profuse night sweats, and Extreme and unexplained tiredness.")
+   st.text("AIDS is a chronic immune system disease caused by the human immunodeficiency virus (HIV). HIV damages the immune system and interferes with the body's ability to fight infection and disease.")
+   st.text("There's no cure for HIV/AIDS, but medications can control the infection and prevent disease progression.")
    st.image("https://static.streamlit.io/examples/dog.jpg")
    
    st.header("Diabetes")
-   st.text("")
-   st.text("")
-   st.text("")
+   st.text("Three common symptoms of diabetes are excessive thirst, frequent urination, and unexplained weight loss.")
+   st.text("Diabetes is a chronic condition characterized by high blood sugar levels resulting from the body's inability to properly produce or use insulin.")
+   st.text("Three ways to prevent diabetes include maintaining a healthy weight through regular physical activity and a balanced diet, avoiding excessive consumption of sugary foods and drinks, and getting regular check-ups to monitor blood sugar levels.")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Gastroenteritis")
-   st.text("")
-   st.text("")
-   st.text("")
+   st.text("Some common symptoms of gastroenteritis include nausea, vomiting, and diarrhea.")
+   st.text("Gastroenteritis is an inflammation of the stomach and intestines that leads to symptoms such as stomach pain, vomiting, and diarrhea.")
+   st.text("To prevent gastroenteritis, it is important to practice good hand hygiene by washing hands thoroughly with soap and water, avoid consuming contaminated food or water, and maintain proper food storage and preparation techniques to prevent bacterial contamination.")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Bronchial Asthma")
-   st.text("")
-   st.text("")
-   st.text("")
+   st.text("Some common symptoms of bronchial asthma include wheezing, shortness of breath, and coughing.")
+   st.text("Bronchial asthma is a chronic respiratory condition characterized by inflammation and narrowing of the airways, leading to difficulty in breathing.")
+   st.text("To prevent bronchial asthma, it is essential to avoid triggers such as allergens (e.g., pollen, dust mites), tobacco smoke, and air pollution. Maintaining a clean and dust-free environment.")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Hypertension")
-   st.text("")
-   st.text("")
-   st.text("")
+   st.text("Some common symptoms of hypertension (high blood pressure) include headaches, dizziness, and shortness of breath.")
+   st.text("Hypertension is a medical condition characterized by persistently elevated blood pressure levels, which can lead to increased strain on the heart and blood vessels.")
+   st.text("To prevent hypertension, it is important to maintain a healthy lifestyle by adopting a balanced diet low in sodium and high in fruits, vegetables, and whole grains, engaging in regular physical activity, and managing stress levels.")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Migraine")
-   st.text("")
-   st.text("")
-   st.text("")
+   st.text("Some common symptoms of migraines include severe headaches, sensitivity to light and sound, and nausea.")
+   st.text("Migraines are a neurological condition characterized by recurrent, pulsating headaches that can be debilitating and are often accompanied by other symptoms.")
+   st.text("Some ways to prevent migraines include identifying and avoiding triggers such as stress, certain foods, and lack of sleep, maintaining a regular sleep schedule, managing stress levels, and discussing with a healthcare professional for preventive medication options.")
    st.image("https://static.streamlit.io/examples/cat.jpg")
 
 with col3:
    st.header("Cervical Spondylosis")
-   st.text("")
-   st.text("")
-   st.text("")
+   st.text("Some common symptoms of cervical spondylosis include neck pain, stiffness, and numbness or weakness in the arms or hands.")
+   st.text("Cervical spondylosis is a degenerative condition that affects the joints and discs in the neck, leading to pain and other associated symptoms.")
+   st.text("Some ways to prevent cervical spondylosis include maintaining good posture, especially while sitting or working for long periods, engaging in regular exercise that includes neck stretches and strengthening exercises, and avoiding activities that strain the neck.")
    st.image("https://static.streamlit.io/examples/owl.jpg")
    
    st.header("Paralysis")
-   st.text("")
-   st.text("")
-   st.text("")
+   st.text("Some common symptoms of paralysis caused by brain hemorrhage include a loss of movement, sensation, or function in certain parts of the body, depending on the area of the brain affected.")
+   st.text("Paralysis due to brain hemorrhage occurs when there is bleeding in the brain, leading to damage and disruption of normal brain function.")
+   st.text("Some ways to prevent brain hemorrhage-related paralysis include maintaining a healthy lifestyle, managing high blood pressure, and avoiding smoking")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Jaundice")
-   st.text("")
-   st.text("")
-   st.text("")
+   st.text("Some common symptoms of jaundice include yellowing of the skin and eyes, dark urine, and fatigue.")
+   st.text("Jaundice is a condition characterized by the yellow discoloration of the skin and other tissues, usually caused by an excess of bilirubin in the blood.")
+   st.text("Some ways to prevent jaundice include practicing good hygiene, ensuring safe drinking water, maintaining a clean and sanitary environment, getting vaccinated against hepatitis, and following safe food handling practices.")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Malaria")
-   st.text("")
-   st.text("")
-   st.text("")
+   st.text("Some common symptoms of malaria include fever, chills, headache, and body aches.")
+   st.text("Malaria is a mosquito-borne infectious disease caused by parasites of the Plasmodium genus and can lead to severe illness or even death if not promptly treated.")
+   st.text("Some ways to prevent malaria include using mosquito nets while sleeping, wearing protective clothing, using mosquito repellents, removing stagnant water sources, and taking antimalarial medications if recommended for the region you are in.")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Chicken Pox")
-   st.text("")
-   st.text("")
-   st.text("")
+   st.text("Some common symptoms of chickenpox include an itchy rash, fever, and flu-like symptoms.")
+   st.text("Chickenpox is a highly contagious viral infection characterized by a blister-like rash that appears all over the body.")
+   st.text("Some ways to prevent chickenpox include getting vaccinated with the varicella vaccine, avoiding close contact with infected individuals, practicing good hygiene, and covering the mouth and nose while coughing or sneezing.")
    st.image("https://static.streamlit.io/examples/cat.jpg")
    
    st.header("Dengue")
+   st.text("Some common symptoms of dengue include high fever, severe headache, joint and muscle pain, and rash.")
+   st.text("Dengue is a mosquito-borne viral infection that can cause flu-like symptoms and, in severe cases, lead to dengue hemorrhagic fever or dengue shock syndrome.")
+   st.text("Some ways to prevent dengue include eliminating mosquito breeding sites, using mosquito repellents, wearing protective clothing, and using bed nets. Additionally, practicing good hygiene and seeking medical care for suspected cases can help prevent complications.")
    st.image("https://static.streamlit.io/examples/cat.jpg")
+   
 
 

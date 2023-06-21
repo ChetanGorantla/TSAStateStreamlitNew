f44594462a4e73cc2fb6f439dfbe9970a52266fb
ChetanGorantla <65378979+ChetanGorantla@users.noreply.github.com>
2023-06-21 14:34:12 -0500

    Update 03_🧠 Stroke.py

--- pages/03_🧠 Stroke.py
+++ pages/03_🧠 Stroke.py
@@ -10,3 +10,13 @@ footer {visibility: hidden;}
 </style> """, unsafe_allow_html=True)
 st.markdown("# Stroke Predictor")
 st.write("Input your symptoms below")
+
+age = st.number_input("How old are you?", value=25, min_value=25, max_value=70) 
+option = st.selectbox(
+    'What is your sex?',
+    ('Male', 'Female'))
+
+col1, col2 = st.columns(2)
+
+hypertension = col1.checkbox("Do you have hypertension?")
+heart_disease = col1.checkbox("Do you have heart disease?")

c9646343ef68a7c8aa617cece92cb009cccb7787
Zarik Khan <70036299+ZK333@users.noreply.github.com>
2023-06-21 23:05:51 -0500

    Update 03_🧠 Stroke.py

--- pages/03_🧠 Stroke.py
+++ pages/03_🧠 Stroke.py
@@ -10,44 +10,63 @@ st.markdown(""" <style>
 footer {visibility: hidden;}
 </style> """, unsafe_allow_html=True)
 
-st.markdown("# Stroke Predictor")
-st.write("Input your symptoms below")
+# Load the trained model
+model = pickle.load(open('model.sav', 'rb'))
 
-#Inputs
-age = st.number_input("How old are you?", value=25, min_value=25, max_value=100) 
-option = st.selectbox(
-    'What is your sex?',
-    ('Male', 'Female'))
+def predict_stroke(features):
+    features = np.array(features).reshape(1, -1)
+    prediction = model.predict(features)
+    probability = model.predict_proba(features)[0][1]
+    return prediction, probability
 
-#Initializing columns
-col1, col2 = st.columns(2)
+def main():
+    st.title("Stroke Prediction")
 
-#Inputs Column 1
-avg_glucose_level = col1.number_input("What is your average glucose level?", value = 0, min_value = 0, max_value = 300)
-bmi = col1.number_input("What is your BMI (Body Mass Index)?", value = 20, min_value = 20, max_value = 40)
-smoking_status = col1.selectbox("What is your smoking status?", ("Never smokes", "Formerly smoked", "Smokes", "Unknown"))
+    age = st.number_input("Age", min_value=1, max_value=100, value=30)
+    hypertension = st.selectbox("Hypertension", ("Yes", "No"))
+    heart_disease = st.selectbox("Heart Disease", ("Yes", "No"))
+    avg_glucose_level = st.number_input("Average Glucose Level", min_value=0.0, value=80.0)
+    bmi = st.number_input("BMI", min_value=0.0, value=20.0)
+    gender = st.selectbox("Gender", ("Male", "Female"))
+    smoking_status = st.selectbox("Smoking Status", ("Unknown", "Formerly Smoked", "Never Smoked", "Smokes"))
+    ever_married = st.selectbox("ever_married", ("Yes", "No"))
+    work_type = st.selectbox("work_type Status", ("Private", "Self-employed", "children", "Govt_job","Never_worked"))
+    Residence_type = st.selectbox("Residence_type", ("Urban", "Rural"))
 
-#Inputs Column 2
-work_type = col2.selectbox("What is your form of work?", ("Private", "Self-employed", "Govt. job"))
-residence_type = col2.selectbox("What is your area of residency?", ("Urban","Rural"))
-hypertension = col2.checkbox("Do you have hypertension?")
-heart_disease = col2.checkbox("Do you have heart disease?")
-married = col2.checkbox("Are you married?")
+    hypertension = 1 if hypertension == "Yes" else 0
+    heart_disease = 1 if heart_disease == "Yes" else 0
+    gender = 1 if gender == "Male" else 0
+    ever_married = 1 if ever_married == "Yes" else 0
+    Residence_type = 1 if gender == "Urban" else 0
 
-predict = col1.button("Predict")
+    smoking_map = {
+        "Unknown": 0,
+        "Formerly Smoked": 1,
+        "Never Smoked": 2,
+        "Smokes": 3
+    }
+    smoking_status = smoking_map[smoking_status]
 
+    work_type_map = {
+        "Govt_job": 0,
+        "Never_worked": 1,
+        "Private": 2,
+        "Self-employed": 3,
+        "children": 4,
+    }
+    work_type = work_type_map[work_type]
 
+    if st.button("Predict Stroke"):
+        features = [age, hypertension, heart_disease, avg_glucose_level, bmi, gender, smoking_status, ever_married, work_type, Residence_type]
 
+        prediction, probability = predict_stroke(features)
 
+        if prediction[0] == 0:
+            st.write("Congratulations! You have a low risk of stroke.")
+        else:
+            st.write("Warning! You are at a high risk of stroke.")
+            st.write("Probability of stroke:", probability)
 
-#styling
-streamlit_style = """
-			<style>
-			@import url('https://fonts.googleapis.com/css2?family=Raleway:ital@1&display=swap');
-			html, body, [class*="css"]  {
-			font-family: 'Raleway', sans-serif;
-			}
-			</style>
-			"""
-st.markdown(streamlit_style, unsafe_allow_html=True)
-
+# Run the web app
+if __name__ == "__main__":
+    main()

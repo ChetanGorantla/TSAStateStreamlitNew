c52420ca0ee9881e63b3f400edd706ca5d8f387e
Zarik Khan <70036299+ZK333@users.noreply.github.com>
2023-06-22 20:19:01 -0500

    Update 03_🧠 Stroke.py

--- pages/03_🧠 Stroke.py
+++ pages/03_🧠 Stroke.py
@@ -1,31 +1,52 @@
 import streamlit as st
-import numpy as np
-import string
+import joblib
+import pandas as pd
 import pickle
-st.set_option('deprecation.showfileUploaderEncoding',False) 
-model = pickle.load(open('stroke2.pkl','rb'))
 
+#Page details
 st.set_page_config(page_title="Diabetes Predictor", page_icon="🧠")
 st.markdown(""" <style>
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
 </style> """, unsafe_allow_html=True)
-st.markdown("# Diabetes  Predictor")
+
+st.markdown("# Stroke Predictor")
 st.write("Input your symptoms below")
-age = st.slider("Input Your age", 0, 120)
-hypertension = st.checkbox("Do you suffer from hypertension?")
-heartdisease = st.checkbox("Do you suffer from heartdisease?")
-sugar = st.slider("Please input your most recent measured glucose level",150.0, 300.000)
-bmi = st.slider("Please input your BMI",0.0,70.0)
 
-inputs = [[age,hypertension,heartdisease,sugar,bmi]]
+#Inputs
+age = st.number_input("How old are you?", value=25, min_value=25, max_value=100) 
+option = st.selectbox(
+    'What is your sex?',
+    ('Male', 'Female'))
 
-if st.button('Predict'):
-  result = model.predict(inputs)
-  updated_res = result.flatten().astype(int)
-  if updated_res == 0:
-    st.write("You are unlikely to suffer from a stroke")
-  else:
-    st.write("You are at risk of suffering from a stroke")
-if __name__ =='__main__':
-  main()
+#Initializing columns
+col1, col2 = st.columns(2)
+
+#Inputs Column 1
+avg_glucose_level = col1.number_input("What is your average glucose level?", value = 0, min_value = 0, max_value = 300)
+bmi = col1.number_input("What is your BMI (Body Mass Index)?", value = 20, min_value = 20, max_value = 40)
+smoking_status = col1.selectbox("What is your smoking status?", ("Never smokes", "Formerly smoked", "Smokes", "Unknown"))
+
+#Inputs Column 2
+work_type = col2.selectbox("What is your form of work?", ("Private", "Self-employed", "Govt. job"))
+residence_type = col2.selectbox("What is your area of residency?", ("Urban","Rural"))
+hypertension = col2.checkbox("Do you have hypertension?")
+heart_disease = col2.checkbox("Do you have heart disease?")
+married = col2.checkbox("Are you married?")
+
+predict = col1.button("Predict")
+
+
+
+
+
+#styling
+streamlit_style = """
+			<style>
+			@import url('https://fonts.googleapis.com/css2?family=Raleway:ital@1&display=swap');
+			html, body, [class*="css"]  {
+			font-family: 'Raleway', sans-serif;
+			}
+			</style>
+			"""
+st.markdown(streamlit_style, unsafe_allow_html=True)

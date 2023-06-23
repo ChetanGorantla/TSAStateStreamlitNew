143cbec20a7dc10a5844b678d144cda253e96740
Zarik Khan <70036299+ZK333@users.noreply.github.com>
2023-06-22 20:15:17 -0500

    Update 03_🧠 Stroke.py

--- pages/03_🧠 Stroke.py
+++ pages/03_🧠 Stroke.py
@@ -18,15 +18,15 @@ heartdisease = st.checkbox("Do you suffer from heartdisease?")
 sugar = st.slider("Please input your most recent measured glucose level",150.0, 300.000)
 bmi = st.slider("Please input your BMI",0.0,70.0)
 
-  inputs = [[age,hypertension,heartdisease,sugar,bmi]]
+inputs = [[age,hypertension,heartdisease,sugar,bmi]]
 
-  if st.button('Predict'):
-    result = model.predict(inputs)
-    updated_res = result.flatten().astype(int)
-    if updated_res == 0:
-       st.write("You are unlikely to suffer from a stroke")
-    else:
-       st.write("You are at risk of suffering from a stroke")
+if st.button('Predict'):
+  result = model.predict(inputs)
+  updated_res = result.flatten().astype(int)
+  if updated_res == 0:
+    st.write("You are unlikely to suffer from a stroke")
+  else:
+    st.write("You are at risk of suffering from a stroke")
    
 
 

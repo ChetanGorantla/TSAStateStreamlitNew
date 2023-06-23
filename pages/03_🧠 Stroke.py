b0f6df74f5a8a950a9741d7cb26639fd740c3e16
Zarik Khan <70036299+ZK333@users.noreply.github.com>
2023-06-23 05:18:38 -0500

    Update 03_🧠 Stroke.py

--- pages/03_🧠 Stroke.py
+++ pages/03_🧠 Stroke.py
@@ -61,4 +61,15 @@ work_govt_job = 1 if work_type == 'Govt. job' else 0
 residence_urban = 1 if residence_type == 'Urban' else 0
 residence_rural = 1 if residence_type == 'Rural' else 0
 
-input_data = np.array([[age, sex_male, sex_female, avg_glucose_level, bmi, smoking_never, smoking_former, smoking_smokes, smoking_unknown, work_private, work_self_employed, work_govt_job, residence_urban, residence_rural, hypertension, heart_disease,
+input_data = np.array([[age, sex_male, sex_female, avg_glucose_level, bmi, smoking_never, smoking_former, smoking_smokes, smoking_unknown, work_private, work_self_employed, work_govt_job, residence_urban, residence_rural, hypertension, heart_disease, married]], dtype=object)
+input_data[:, [0, 3, 4, 8, 9, 10, 11, 12, 13, 14, 15, 16]] = input_data[:, [0, 3, 4, 8, 9, 10, 11, 12, 13, 14, 15, 16]].astype(float)
+
+if predict:
+    try:
+        prediction = model.predict(input_data)
+        if prediction[0] == 1:
+            st.write("The model predicts that you are likely to have a stroke.")
+        else:
+            st.write("The model predicts that you are unlikely to have a stroke.")
+    except Exception as e:
+        st.error(f"Error predicting stroke: {e}")

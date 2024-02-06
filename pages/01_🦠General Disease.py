b19b465f9a65e58d5989ff0be08943122968c92b
ChetanGorantla <65378979+ChetanGorantla@users.noreply.github.com>
2024-02-05 19:39:00 -0600

    Update 01_🦠General Disease.py

--- pages/01_🦠General Disease.py
+++ pages/01_🦠General Disease.py
@@ -445,7 +445,7 @@ if st.button("Predict"):
     )
     st.write(
         """<h1 style="text-align:center"><span style="font-family:
-	Comic Sans MS"><span style="background-color:#e74c3c">"""
+	Marker Felt"><span style="background-color:#e74c3c">"""
         + str(predictDisease(finalstr))
         + """</span></span></h1>
 	""",
